116e2870053ce38b5aa154da80de9562afed11e5
mckaymac <54556260+mckaymac@users.noreply.github.com>
2020-04-30 19:33:12 -0400

    Updating player script for components

--- CabinIchabod/Assets/Scripts/Player.cs
+++ CabinIchabod/Assets/Scripts/Player.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
@@ -13,6 +14,12 @@ public class Player : MonoBehaviour
 
     public AudioSource Hurt;
     private int goal;
+    private Text Salt;
+    public GameObject SaltObject;
+    private Text Bone;
+    public GameObject BoneObject;
+    private Text Rock;
+    public GameObject RockObject;
     // Start is called before the first frame update
     void Start()
     {
@@ -28,7 +35,27 @@ public class Player : MonoBehaviour
         }
     }
 
-    void On
+    void OnCollisionEnter(Collider other){
+        if(other.gameObject.CompareTag("Salt")){
+            other.gameObject.SetActive(false);
+            //add text
+        }
+
+        if(other.gameObject.CompareTag("Bone")){
+            other.gameObject.SetActive(false);
+
+        }
+
+        if(other.gameObject.CompareTag("Rock")){
+            other.gameObject.SetActive(false);
+
+        }
+
+        if(other.gameObject.CompareTag("Goal") && (goal == 3)){
+            //SceneManager.LoadScene("You Win");
+        }
+
+    }
 
     // Update is called once per frame
     void Update()
